c9e8cce2cc855e4a05ec9e17097234b8cece15a3
j5ik2o <j5ik2o@users.noreply.github.com>
2023-07-24 08:19:21 +0000

    deploy: 13e96eae1464689b5883a5e4b72823ecefd21eb7

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.5+5-694957b7-SNAPSHOT',
+    VERSION: '1.0.5+6-13e96eae-SNAPSHOT',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
